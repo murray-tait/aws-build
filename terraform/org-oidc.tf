e7c6ea3745611a4d0deff63e239466e2767bbd61
Murray Tait <6074789+deathtumble@users.noreply.github.com>
2025-11-02 21:49:49 +0000

    Add oidc config for org repository

---
+++ terraform/org-oidc.tf
@@ -0,0 +1,64 @@
+resource "aws_iam_role" "org" {
+  name = "Github-Actions-OIDC-org"
+
+  assume_role_policy = jsonencode({
+    Version : "2012-10-17",
+    Statement : [
+      {
+        Effect : "Allow",
+        Principal : {
+          Federated : "arn:aws:iam::973963482762:oidc-provider/token.actions.githubusercontent.com"
+        },
+        Action : "sts:AssumeRoleWithWebIdentity",
+        Condition : {
+          StringEquals : {
+            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
+          },
+          StringLike : {
+            "token.actions.githubusercontent.com:sub" : "repo:murray-tait/aws-org:*:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "org" {
+  name = "Terraform_Backend-org"
+  role = aws_iam_role.org.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = "s3:ListBucket"
+        Effect   = "Allow"
+        Resource = "arn:aws:s3:::org.murraytait.experiment.build.terraform"
+      },
+      {
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+        ]
+        Effect = "Allow"
+        Resource = [
+          "arn:aws:s3:::org.murraytait.experiment.build.terraform/env/aws-org/terraform.tfstate",
+        ]
+      },
+      {
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject",
+        ]
+        Effect = "Allow"
+        Resource = [
+          "arn:aws:s3:::org.murraytait.experiment.build.terraform/env/aws-org/terraform.tfstate.tflock",
+        ]
+      },
+    ]
+  })
+}
+
